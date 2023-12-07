d95f5411a6a2fe3b5b4307318dbb297d87d9eaa2
kokko.sampo@gmail.com <kokko.sampo@gmail.com>
2023-12-07 19:34:00 +0200

    day5 finally working (I will be unit testing in the future)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::{time::Instant, fs, cmp::{min, max}, ops::RangeFrom};
+use std::{time::Instant, fs, cmp::min};
 
 type Maps = Vec<Vec<Map>>;
 type Map = (u64, u64, u64);
@@ -49,6 +49,8 @@ fn read_input(filename: &str) -> (Vec<u64>, Maps) {
         }
     });
 
+    //sort the maps by the start of the source range
+    maps.iter_mut().for_each(|c| c.sort_by_key(|k| k.1));
     (seeds, maps)
 }
 
@@ -67,9 +69,6 @@ fn find_location(seed: &u64, maps: &Maps) -> u64 {
         // source numbers that aren't mapped correspond to the same destination
         // number, so we don't need to do anything here
     });
-    if true {
-        println!("{path:?}");
-    }
     val
 }
 
@@ -81,89 +80,79 @@ fn get_seed_ranges(seeds: &Vec<u64>) -> Vec<(u64, u64)> {
     //I dont like the format of (start, length), so we'll convert the ranges 
     //to (start, start+length)
     starts.iter().zip(lengths.iter()).map(|(a, b)| {
-        (a, a + b)
+        (a.to_owned(), (a + b).to_owned())
     })
     .collect()
 }
 
 //Turns the map into the format (start, end, difference) so my brain can 
 //read it
-fn get_range(map: (u64, u64, u64)) -> (u64, u64, i32) {
-    (map.1, map.1 + map.2, map.2 - map.1)
+fn get_range(map: (u64, u64, u64)) -> (u64, u64, i64) {
+    (map.1, map.1 + map.2 - 1, map.0 as i64 - map.1 as i64)
 }
 
-fn get_next_layer(prev: &Vec<(u64, u64)>, category: &Vec<(u64, u64, u64)>) -> Vec<(u64, u64, u64)> {
+fn get_next_layer(prev: &Vec<(u64, u64)>, category: &Vec<(u64, u64, u64)>) -> Vec<(u64, u64)> {
+    let mut next_layer: Vec<(u64, u64)> = Vec::new();
 
-}
+    let category: Vec<(u64, u64, i64)> = category.iter().map(|c| get_range(*c)).collect();
 
-fn get_candidates(seed_ranges: &Vec<(u64, u64)>, maps: &Maps) -> Vec<(u64, u64)> {
-    let mut candidates = seed_ranges.clone();
-    maps.iter().for_each(|m| {
-        let mut new_candidates = candidates.clone();
-        candidates.for_each(|c| {
-            match compare_ranges((m.1, m.1 + m.2), (c.0, c.0 + c.1)) {
-                Inside => new_candidates.push((c.0 + (m.1 - m.0)
-
-
-    candidates
+    let mut _flag = true;
+    prev.iter().for_each(|l| {
+        let mut remaining_range = Some(*l);
+        category.iter().for_each(|c| {
+            if let Some(r) = remaining_range {
+                if let Some(mapped_range) = map_range(&r, c) {
+                    remaining_range = mapped_range.1;
+                    mapped_range.0.iter().for_each(|a| {
+                        next_layer.push(*a);
+                    });
+                };
+            }
+        });
+        if let Some(r) = remaining_range {
+            next_layer.push(r)
+        }
+    });
+    next_layer.sort_by_key(|k| k.0);
+    next_layer
 }
 
 //Creates new ranges of seeds based on the overlap of the seed range and map
-fn map_range(seeds: (u64, u64), map: (u64, u64, i32)) -> Option<Vec<(u64, u64)>> {
+//If the range of seeds and the map overlap, and the range of seeds exceeds 
+//beyond the range of the map, return the exceeding positions separately
+fn map_range(seeds: &(u64, u64), map: &(u64, u64, i64))
+    -> Option<(Vec<(u64, u64)>, Option<(u64, u64)>)> {
     if seeds.1 < map.0 || seeds.0 > map.1 {
-        None
+        return None;
     }
-    match (seeds.0 <= map.0, seeds.1 <= map.1) {
+    // pray to the lord we don't get overflows here
+    match (seeds.0 < map.0, seeds.1 > map.1) {
         (true, true) => {
-            vec![(seeds.0, map.0 - 1), (map.0 + map.2, seeds.1 + map.2)]
+            Some((vec![(seeds.0, map.0 - 1),
+            (map.0.wrapping_add_signed(map.2), map.1.wrapping_add_signed(map.2))],
+            Some((map.1 + 1, seeds.1))))
         },
         (true, false) => {
-            vec![(seeds.0, map.0 - 1),
-            (map.0 + map.2, map.1 + map.2),
-            (map.2 + 1, seeds.1)]
+            Some((vec![(seeds.0, map.0 - 1), (map.0.wrapping_add_signed(map.2), seeds.1.wrapping_add_signed(map.2))], None))
         },
         (false, true) => {
-            vec![(seeds.0 + map.2, 
+            Some((vec![(seeds.0.wrapping_add_signed(map.2), map.1.wrapping_add_signed(map.2))], Some((map.1 + 1, seeds.1))))
         },
         (false, false) => {
-
+            Some((vec![(seeds.0.wrapping_add_signed(map.2), seeds.1.wrapping_add_signed(map.2))], None))
         }
-}
-
-
-fn get_lowest_candidates(seed_ranges: &Vec<(u64, u64)>, maps: &Maps) -> Vec<u64> {
-    let mut candidates = vec![];
-    maps.iter().for_each(|c| {
-        c.iter().for_each(|m| {
-            seed_ranges.iter().for_each(|s| {
-                if m.1 >= s.0 && m.1 <= s.0 + s.1 {
-                    candidates.push(m.1);
-                }
-
-                if m.0 >= s.0 && m.0 <= s.0 + s.1 {
-                    candidates.push(m.0);
-                }
-            })
-        })
-    });
-    let min_candidate = candidates.iter().min().unwrap().clone();
-    seed_ranges.iter().for_each(|r| {
-        if r.0 < min_candidate {
-            (r.0..min(min_candidate, r.1)).for_each(|s| {
-                candidates.push(s);
-            });
-        }
-    });
-    candidates
+    }
 }
 
 fn main() {
+    //Read input
     let now = Instant::now();
-    let (seeds, maps) = read_input("testinput.txt");
+    let (seeds, maps) = read_input("input.txt");
     println!("number of seeds: {}", seeds.len());
     let read_elapsed = now.elapsed();
     println!("Time taken to read data into maps: {read_elapsed:?}");
     
+    //part 1
     let now = Instant::now();
     let min_location = seeds.iter().map(|s| {
         find_location(s, &maps)
@@ -172,29 +161,22 @@ fn main() {
     .unwrap();
     let part1_elapsed = read_elapsed + now.elapsed();
     println!("Total time taken to complete part 1: {:?}", part1_elapsed);
-    println!("Lowest location number: {min_location}");
+    println!("Lowest location number: {min_location}\n");
 
     let now = Instant::now();
 
     //part2 
-    let seeds = get_seed_ranges(seeds).iter().map(|r| {
-        get_range(r)
-    })
-    .collect()
-    .sort_by_key(|r| r.0);
+    //___________________________REMOVE
+    let seeds = get_seed_ranges(&seeds);
+    let mut count = 1;
 
-    let mut ranges = vec![];
-    maps.iter().map(|c| {
-        let mut next_layer = vec![];
-        c.iter().for_each(|m| {
-            next_layer.push(get_range(m));
-        });
-        ranges.push(next_layer.sort_by_key(|r| r.0));
+    let mut layer = seeds;
+    maps.iter().for_each(|c| {
+        layer = get_next_layer(&layer, c);
+        count += 1;
     });
-
-
     
     let part2_elapsed = read_elapsed + now.elapsed();
     println!("Total time taken to complete part 1: {:?}", part2_elapsed);
-    println!("Lowest location number: {min_location}");
+    println!("Lowest range: {:#?}", layer.iter().min().unwrap());
 }
