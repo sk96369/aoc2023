d80692ca41b43ad7c002e75bcc545e1159be5ba9
kokko.sampo@gmail.com <kokko.sampo@gmail.com>
2023-12-07 23:28:01 +0200

    day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{BinaryHeap, HashMap}, fs, cmp::{Ordering, Reverse}};
+use std::{time::{self, Instant}, collections::{BinaryHeap, HashMap}, fs, cmp::Ordering};
 
 #[derive(Eq, Debug)]
 struct Hand {
@@ -64,6 +64,35 @@ fn read_input(filename: &str) -> Vec<Hand> {
     ordered_hands.into_sorted_vec()
 }
 
+fn read_input_2(filename: &str) -> Vec<Hand> {
+    let char_to_card: HashMap<char, u64> = HashMap::from([
+        ('2', 2),
+        ('3', 3),
+        ('4', 4),
+        ('5', 5),
+        ('6', 6),
+        ('7', 7),
+        ('8', 8),
+        ('9', 9),
+        ('T', 10),
+        ('J', 1),
+        ('Q', 12),
+        ('K', 13),
+        ('A', 14)
+    ]);
+
+    let mut ordered_hands = BinaryHeap::new();
+    fs::read_to_string(filename).unwrap().lines().filter(|l| !l.is_empty())
+        .for_each(|s| {
+            let hand: Vec<&str> = s.split_whitespace().collect();
+            ordered_hands.push(Hand::new(hand[0].chars().map(|c| {
+                char_to_card.get(&c).unwrap().to_owned()
+            }).collect(),
+                hand[1].parse().unwrap()))
+    });
+    ordered_hands.into_sorted_vec()
+}
+
 //Returns a score, which is calculated by the poker rules, and a hash value
 //appended to it
 fn score_hand(cards: &Vec<u64>) -> u64 {
@@ -74,6 +103,27 @@ fn score_hand(cards: &Vec<u64>) -> u64 {
         score += (100_u64.pow(i as u32)) * c;
         counts.entry(*c).and_modify(|sum| *sum += 1).or_insert(1);
     });
+
+    //If there are jokers in the hand, treat them as the card with the highest
+    //count, because this always produces the best hand
+    let jokers = match counts.remove(&1) {
+        Some(v) => v,
+        None => 0,
+    };
+
+    if jokers > 0 {
+        counts.entry(1).and_modify(|v| *v = 0);
+        let other = match counts.keys().map(|k| (k, counts.get(k).unwrap()))
+            .max_by_key(|(_, c)| *c) {
+                Some(c) => (*c.0, *c.1),
+                None => {
+                    //This happens if there are 5 jokers in the hand
+                    counts.insert(1, 0);
+                    (1, 1)
+                },
+            };
+        counts.entry(other.0).and_modify(|v| *v += jokers);
+    }
 
     score += 10_u64.pow(10) * match counts.len() {
         1 => 6,
@@ -113,7 +163,16 @@ fn calculate_scores(hands: Vec<Hand>) -> Vec<u64> {
 
 
 fn main() {
+    let part1_time = Instant::now();
     let hands = read_input("input.txt");
     let scores = calculate_scores(hands);
     println!("Total score = {}", scores.iter().sum::<u64>());
+    println!("Time elapsed for part 1: {}", part1_time.elapsed().as_micros());
+    let part2_time = Instant::now();
+    
+    let hands = read_input_2("input.txt");
+    let scores = calculate_scores(hands);
+    println!("Total score = {}", scores.iter().sum::<u64>());
+    println!("Time elapsed for part 2: {}", part2_time.elapsed().as_micros());
+
 }
